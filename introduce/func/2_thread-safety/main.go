c6575f2a0aae97c982fda86747c74df208b3a26d
theeraphat.ch <ots.theeraphat.ch>
2026-06-02 08:42:07 +0700

    feat: reorganize project

---
+++ introduce/func/2_thread-safety/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type SafeCounter struct {
+	mu    sync.Mutex
+	count int
+}
+
+func (c *SafeCounter) Inc() {
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+}
+
+func (c *SafeCounter) Value() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.count
+}
+
+func main() {
+	c := &SafeCounter{}
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			c.Inc()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Result:", c.Value())
+}
